d801f27dac57277e829b4d66879fd1245cbd68cd
Abdul Azeem <azeem3830408@gmail.com>
2022-11-29 15:18:57 +0530

    updated the batch file

--- Git_example/testbatch.bat
+++ Git_example/testbatch.bat
@@ -1,4 +1,4 @@
-set projectLocation=Git_Example
+set projectLocation=Git_example
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*;
 java org.testng.TestNG testng.xml
